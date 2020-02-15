d406f8502b5fe67deb97cffe5eb677cf8e1db232
robertalanis <rxalanis@gmail.com>
2020-02-14 21:22:37 -0600

    js variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,30 @@
-alert("TEST!")
-console.log(5+5)
+// word list
+var words = ["january", "february", "march", "april", "may"];
+
+// maximum number of guesses
+var maxGuesses = 10;
+
+// array to store letters that have been guessed
+var lettersGuessed = [];
+
+// store the empty letter placeholder
+var ansWordArr = [];
+
+// number of guesses remaining
+var numGuessesRemaining = 0;
+
+// number of wins
+var numWins = 0;
+
+// number of nosses
+var numLosses = 0;
+
+// then true, game can start again
+var isFinished = false;
+
+// the word that is being played
+var ansWord;
+
+
+
+
