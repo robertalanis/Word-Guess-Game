a89312194d511906944604e3d967fc7e1e70943a
robertalanis <rxalanis@gmail.com>
2020-02-15 00:25:14 -0600

    debugged JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,106 @@
 // word list
-var wordList = ["january", "february", "march", "april", "may"];
+var wordList = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY"];
 
 // maximum number of guesses
 var maxGuesses = 10;
-
 // array to store letters that have been guessed
 var lettersGuessed = [];
-
 // store the empty letter placeholder
 var wordAnsArr = [];
-
 // number of guesses remaining
 var numGuessesRemaining = 0;
-
 // number of wins
 var numWins = 0;
-
-// number of nosses
+// number of losses
 var numLosses = 0;
-
 // then true, game can start again
 var isFinished = false;
-
 // the word that is being played
 var wordAns;
 
 
-
-// function runs at the start of the page
+// function runs at start of the page
 // function also used to restart after game is finished
 function setup() {
 
-    //picks random word from wordList
-    wordAns = wordList[Math.floor(math.random() * wordAns.length)];
-
+    //pick random word from wordList
+    wordAns = wordList[Math.floor(Math.random() * wordList.length)];
     wordAnsArr = [];
-
 
     // adds "_" to wordAnswerArr
     for (var i = 0; i < wordAns.length; i++) {
-        wordAns[i = "_"];
-    }
+        wordAnsArr[i] = "_";
+    };
 
-    // reset the variables
+    // reset variables
     numGuessesRemaining = maxGuesses;
     lettersGuessed = [];
 
-    // code for photos to be displayed with correct answer
-
-    // show the selected elements on the screen
+    // show selected elements on the screen
     updateScreen ();
-}
+};
 
-// updates the HTML from the functions
+// function to update screen
 function updateScreen() {
-    document.getElementById("numWins").innerText = numwins;
+    document.getElementById("numWins").innerText = numWins;
     document.getElementById("numLosses").innerText = numLosses;
     document.getElementById("numGuessesRemaining").innerText = numGuessesRemaining;
     document.getElementById("wordAns").innerText = wordAnsArr.join("");
     document.getElementById("lettersGuessed").innerText = lettersGuessed;
-}
+};
+
+// function to check what key is pressed
+function checkGuess(letter) {
+    // if letter is not in lettersGuessed array then push the letter to the array
+    if (lettersGuessed.indexOf(letter) === -1) {
+        lettersGuessed.push(letter);
+        // subtract numGuessesRemaining if letter not in answer  
+        if (wordAns.indexOf(letter) === -1) {
+            numGuessesRemaining--;
+            // if numGuessesRemaining is 3 or less then change color
+            if (numGuessesRemaining <=3) {
+                document.getElementById("numGuesses").style.color = "#CD5C52"    
+            }
+        } else {
+            for (var i = 0; i < wordAns.length; i++) {
+                if (letter === wordAns[i]) {
+                    wordAnsArr[i] = letter;
+                }
+            }
+        }
+    }
+};
+
+
+function isWinner() {
+    if (wordAnsArr.indexOf("_") === -1) {
+        numWins++;
+        isFinished = true;
+    }
+};
+
+function isLoser() {
+    if(numGuessesRemaining <= 0) {
+        numLosses++;
+        isFinished = true;
+    }
+};
+
+document.onkeyup = function(event) {
+    if (isFinished) {
+        setup();
+        isFinished = false;
+    } else {
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            checkGuess(event.key.toUpperCase());
+            updateScreen();
+            isWinner();
+            isLoser();
+        }
+    }
+};
+
+setup();
+updateScreen();
+
+console.log(ansWord);
